52df6e77d4dc8d6a08b5cf74675ac8e13632d6d4
Peter Ross <peteross@outlook.dk>
2024-11-24 17:01:50 +0100

    dependencies fix

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import React, { useState, useEffect, useContext } from 'react';
 import Header from "./components/Header/Header";
 import Home from './components/pages/Home';
-import {Routes, Route, Router} from 'react-router-dom';
+import {Routes, Route} from 'react-router-dom';
 import { ItemsProvider } from './context/ItemContext';
 import { BasketProvider } from './context/BasketContext';
 function App() {
